b4566a7f3104c420ddff21eef9cd784def3dc8f1
Guenther <gzoppelt@gmail.com>
2019-04-21 05:25:18 +0100

    2:19 allMarkdownRemark

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        allMarkdownRemark {
+            edges {
+              node {
+                frontmatter {
+                  title
+                  date
+                }
+              }
+            }
+          }
+        }
+    `) 
+ 
+    const blogs = data.allMarkdownRemark.edges
+    
     return (
         <Layout>
-            <h1>Blog</h1>
-            <h2>Posts will be show up later here.</h2>
+            <h1>Blogs</h1>
+            <ol>
+                {blogs.map( (blog, index) => {
+                    return (
+                        <li key={index}>  
+                            <h2>{blog.node.frontmatter.title}</h2>
+                            <p>{blog.node.frontmatter.date}</p>
+                        </li>   
+                    )    
+                })}
+            </ol>
         </Layout>
     )
 }
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "The Great Gatsby Bootcamp"
+date: "2019-04-20"
+---
+
+I just started a new bootcamp!
+
+## Topics Covered
+
+1. Gatsby
+2. GraphQl
+3. React
